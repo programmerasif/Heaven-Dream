3e408c4307295bb8c97a1ba03c753d45951b0595
programmerasif <mdasifkhan3757@gmail.com>
2023-10-23 21:44:56 +0600

    added

--- src/app/components/offer/page.js
+++ src/app/components/offer/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Offer = () => {
@@ -9,10 +10,14 @@ const Offer = () => {
 data-aos="fade-up"
 data-aos-duration="1500"
 className='w-full md:w-[60%] text-center  text-gray-100'>Embark on instant savings with a timely reservation. This exclusive, fleeting offer presents a valuable 15% discount. Seize the opportunity now to secure both reduced rates and an unforgettable experience in our Deluxe Dark Room.</p>
+<Link href='/allRooms'>
 <button
 data-aos="fade-up"
 data-aos-duration="2000"
 className='bg-[#ffa000] text-sm text-white px-12 py-4 md:mt-5 mt:2 font-bold rounded-full hover:bg-white hover:text-[#ffa000] duration-300'>Book-Room</button>
+</Link>
+
+
 </div>
         </div>
     );
